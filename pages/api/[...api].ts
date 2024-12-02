88e92fdb7545b58f0a9d399d9788ee967717467a
caesar <454451758@qq.com>
2024-12-02 09:49:35 +0800

    chore: router api locale

--- pages/api/[...api].ts
+++ pages/api/[...api].ts
@@ -5,6 +5,11 @@ import { domainConf } from "@/config/domain.conf";
 
 const { i18n, ip } = domainConf;
 
+const getLocaleCode = (url: string) => {
+  const match = url.match(/^https?:\/\/[^/]+\/([a-z]{2,3})(\/|$)/);
+  return match ? match[1] : null;
+};
+
 const apiProxy: any = createProxyMiddleware({
   changeOrigin: true,
   secure: true,
@@ -12,12 +17,14 @@ const apiProxy: any = createProxyMiddleware({
     "^/api": "",
   },
   router: async (req: any) => {
+    const referer: string = req.headers?.referer ?? "";
+    const locale: string = getLocaleCode(referer) || i18n.defaultLocale;
+    console.info("locale:", locale);
+
     const ipAddress =
       req.headers?.["x-forwarded-for"]?.split(",")[0]?.trim() || // Check for forwarded IP
       req.headers?.["x-real-ip"] || // Alternate forwarded IP header
       "127.0.0.1"; // Default for localhost or unknown
-
-    const locale = req.headers?.["x-locale-code"];
     return `${ip.apiUrl}?apiKey=${ip.apiKey}&ip=${ipAddress}&lang=${locale}`;
   },
 } as any);
